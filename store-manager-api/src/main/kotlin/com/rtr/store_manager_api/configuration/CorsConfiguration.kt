b287bebebc878e0e2a1cd90dc47f3cde44db37cb
RafaelH31 <rafael.cebrian@sptech.school>
2025-09-16 13:20:58 -0300

    feat: cors config

---
+++ store-manager-api/src/main/kotlin/com/rtr/store_manager_api/configuration/CorsConfiguration.kt
@@ -0,0 +1,17 @@
+package com.rtr.store_manager_api.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
