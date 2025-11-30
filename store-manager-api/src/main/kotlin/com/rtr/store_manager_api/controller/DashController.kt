a37ebb9c37b9d9bc2bb78387fd832349edc59945
Edson Morro Nogueira <125297431+EdsonMorroNogueira@users.noreply.github.com>
2025-11-29 22:35:01 -0300

    Change dashboard API endpoint path

--- store-manager-api/src/main/kotlin/com/rtr/store_manager_api/controller/DashController.kt
+++ store-manager-api/src/main/kotlin/com/rtr/store_manager_api/controller/DashController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/dashboard")
+@RequestMapping("/store-manager-api/dashboard")
 class DashController(
     private val dashService: DashService
 ) {
